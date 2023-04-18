fe87d6023725807e78c8e8962b689e489a206677
Philippe SABAA <philippe.sabaa@insee.fr>
2023-04-18 17:15:57 +0200

    Mise en place connexion et test dans le main

--- formation-spring-parent/formation-spring-core/src/main/java/org/formation/spring/core/App.java
+++ formation-spring-parent/formation-spring-core/src/main/java/org/formation/spring/core/App.java
@@ -1,13 +1,30 @@
 package org.formation.spring.core;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import fr.insee.config.InseeConfig;
+import fr.insee.config.exception.PoolException;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) throws PoolException, SQLException {
+		System.out.println("Hello World!");
+
+		Connection con = InseeConfig.getPool().getConnection("database_pool");
+
+		Statement stmt = con.createStatement();
+		ResultSet rs = stmt.executeQuery("select 1");
+
+		if (rs.next())
+			System.out.println("Et le résultat est ... Tadammm : " + rs.getInt(1));
+		else
+			throw new RuntimeException("T'es trop nul ton code il est tout pourri !");
+
+	}
 }

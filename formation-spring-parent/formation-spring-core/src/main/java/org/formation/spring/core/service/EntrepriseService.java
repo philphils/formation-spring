cbd92c7a5326ae009cac060519a11bc5aadf47c0
Philippe SABAA <philippe.sabaa@insee.fr>
2024-03-29 15:06:06 +0100

    Ajout Javadoc

--- formation-spring-parent/formation-spring-core/src/main/java/org/formation/spring/core/service/EntrepriseService.java
+++ formation-spring-parent/formation-spring-core/src/main/java/org/formation/spring/core/service/EntrepriseService.java
@@ -1,8 +1,18 @@
 package org.formation.spring.core.service;
 
 import org.formation.spring.core.persistence.model.Entreprise;
+import org.formation.spring.core.persistence.model.Secteur;
+
+import com.github.javafaker.Faker;
 
 public interface EntrepriseService {
 
+	/**
+	 * Créer une {@link Entreprise}. Si aucun {@link Secteur} n'existe alors un
+	 * {@link Secteur} est créé. Sinon on la rattache à un {@link Secteur} existant.
+	 * Les champs sont rempli aléatoiremeent avec {@link Faker}
+	 * 
+	 * @return
+	 */
 	public Entreprise createRandomEntreprise();
 }
